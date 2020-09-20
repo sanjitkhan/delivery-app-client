b77b80b13d00eb5b9445a3d2c9846bb880b435c1
sanjitkhan <sanjitkhan2194@gmail.com>
2020-09-20 11:12:41 +0530

    feat: init react-intl

---
+++ src/messages/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+    car: {
+        id: 'vehicles.ford',
+        defaultMessage: 'Ford'
+    },
+    bus: {
+        id: 'vehicles.mercedes',
+        defaultMessage: 'Mercedes'
+    }
+});
