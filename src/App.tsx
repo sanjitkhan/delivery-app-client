4c40037dc061618ed9f727d6f194e23a0e3d5636
sanjitkhan <sanjitkhan2194@gmail.com>
2020-09-21 22:42:50 +0530

    feat: init react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,43 @@
 import React from 'react';
-import { Button, Label, Input } from 'semantic-ui-react';
-import { 
-  increaseCounter as increaseCounterAction,
-  decreaseCounter as decreaseCounterAction
-} from './redux/count/actions';
-import { connect } from 'react-redux';
-import { ApplicationState } from './redux/types';
-import { FormattedDate, injectIntl, WrappedComponentProps } from 'react-intl';
-import messages from './messages/messages';
 import styled from 'styled-components';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import DateTime from './containers/DateTime';
+import Counter from './containers/Counter';
+import NavBar from './components/Navbar';
 
 export type GenericCallBack = (...args: any[]) => any;
 
-export interface AppProps extends WrappedComponentProps {
-  count: any;
-  increaseCounter: any;
-  decreaseCounter: any;
-}
-
-interface AppState {
-  increaseBy: number;
-}
-
-const StyledDiv = styled.div`
+export const StyledDiv = styled.div`
   text-align: center;
   background-color: ${props => props.theme.background.pageBackground}
 `;
 
-export class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      increaseBy: 0
-    }
-  }
+export class App extends React.Component {
   render() {
-    const {
-      count: { count },
-      increaseCounter,
-      decreaseCounter,
-      intl: { formatMessage }
-    } = this.props;
-    const { increaseBy } = this.state;
     return (
-      <StyledDiv>
-        <div>abc</div>
-        <Button primary onClick={() => increaseCounter(increaseBy)}>Increase</Button>
-        <Input type="number" value={increaseBy} onChange={(e, { value }) => {
-          this.setState((state) => ({
-            increaseBy: Number(value)
-          }));
-        }}></Input>
-        <Button primary onClick={() => decreaseCounter(increaseBy)}>Decrease</Button>
-        <Label>{count}</Label><br/>
-        <FormattedDate
-          value={new Date(1459913574887)}
-          year="numeric"
-          month="long"
-          day="numeric"
-          weekday="long"
-        />
-        {formatMessage(messages.car)}
-        {formatMessage(messages.bus)}
-      </StyledDiv>
+      <>
+        <div>
+          <NavBar/>
+        </div>
+        <StyledDiv>
+          <Switch>
+            <Route exact path="/counter">
+              <Counter />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/counter" />
+            </Route>
+            <Route exact path="/datetime">
+              <DateTime />
+            </Route>
+            <Route path="*">
+              <div>not found</div>
+            </Route>
+          </Switch>
+        </StyledDiv>
+      </>
     );
   }
 }
 
-function mapStateToProps(
-  state: ApplicationState
-): Partial<AppProps> {
-  const { count } = state;
-  return {
-    count
-  };
-}
-  
-const mapActionToProps: Partial<AppProps> = {
-  increaseCounter: increaseCounterAction,
-  decreaseCounter: decreaseCounterAction
-};
-
-export default connect(
-  mapStateToProps,
-  mapActionToProps
- )(injectIntl(App));
+export default App;
