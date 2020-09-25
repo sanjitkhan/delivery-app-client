df86cef15dbcc3ab6e80b3f14e64528df0495b6e
sanjitkhan <sanjitkhan2194@gmail.com>
2020-09-25 15:32:37 +0530

    feat: new design for category cards

--- src/containers/Items/ItemsTabs/CategoriesTab.tsx
+++ src/containers/Items/ItemsTabs/CategoriesTab.tsx
@@ -1,20 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 import CategoryCard, { backgroundThemeEnum, CategoryCardProps, positionEnum } from "../../../components/CategoryCard/CategoryCard";
 import frozenFoodImg from "../../../assets/images/category_frozenfoods.jpg"
 import iceCreamImg from "../../../assets/images/category_icecream.jpg"
 import confectioneryImg from "../../../assets/images/category_confectionery.jpg"
 import groceriesImg from "../../../assets/images/category_groceries.jpg"
-
-const StyledContainer = styled.div`
-  margin: 20px 20px;
-  > div {
-    margin: 15px 0px;
-  }
-  && div:first-child {
-    margin-top: 0px;
-  }
-`;
 
 const CategoriesTab: React.FC = () => {
   const categories: Omit<CategoryCardProps, 'width'>[] = [
@@ -52,14 +41,14 @@ const CategoriesTab: React.FC = () => {
     }
   ]
   return (
-    <StyledContainer>
+    <div>
       {categories.map(item => (
         <CategoryCard
           key={item.id}
           {...item}
         />
       ))}
-    </StyledContainer>
+    </div>
   );
 }
 
